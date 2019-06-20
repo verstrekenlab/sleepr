d413a27a9b7e62751fe4fde836dc3d6ea268a8dc
Antonio Ortega <ntoniogu@gmail.com>
2019-06-21 00:36:28 +0200

    Fix test for hamming index

--- tests/testthat/test-hamming-index.R
+++ tests/testthat/test-hamming-index.R
@@ -3,17 +3,14 @@ set.seed(42)
 
 
 asleep_sequence1 <- c(0, 1, 1, 1, 0, 1, 1, 1, 0, 0)
-asleep_sequence2 <- c(0, 1, 1, 0, 1, 0, 1, 0, 1, 1)
+long_asleep_sequence <- c(0,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,0,0,1,1,1,0,1,1,1,0)
 
-long_asleep_sequence_file <- system.file("extdata", "long_asleep_sequence.txt", package = "flyr")
-
-long_asleep_sequence <- read.table(file = long_asleep_sequence_file)[,1]
 
 test_that("the hamming index is computed properly", {
   short_hi <- hamming_index(asleep_sequence1)
   long_hi <- hamming_index(long_asleep_sequence)
   expect_equal(short_hi, 0.1)
-  expect_equal(long_hi, 0.24)
+  expect_equal(long_hi, 0.1875)
 })
 
 test_that("if no sleep/doze state is observed, the function returns a as.double(NA)", {
