19d6657e804f0518207e7c94521e06342fa889c3
Antonio <ntoniohu@gmail.com>
2020-06-23 12:24:25 +0000

    Implement motion detector

--- R/custom_annotation.R
+++ R/custom_annotation.R
@@ -1,6 +1,6 @@
 #' @export
 #' @import data.table
-distance_sum <- function(d, time_window_length) {
+distance_sum_enclosed <- function(d, time_window_length) {
   d <- prepare_data_for_motion_detector(d,
                                         c("t", "xy_dist_log10x1000"),
                                         time_window_length)
@@ -12,25 +12,68 @@ distance_sum <- function(d, time_window_length) {
 attr(distance_sum, "needed_columns") <- function(...) {
   c("t", "dist_sum")
 }
-#' @export
-#' @import data.table
-velocity_avg <- function(d, time_window_length) {
 
-  d2 <- copy(d)
-  d2 <- prepare_data_for_motion_detector(d2,
-                                        c("t", "xy_dist_log10x1000"),
-                                        time_window_length)
 
-  d2[, dt := c(NA, diff(t))]
-  d2[, dist := 10**((xy_dist_log10x1000)/1e3)]
-  d2[, velocity :=  dist / dt]
-  # t_round is included in the columns because it is in the by arg
-  d2 <- d2[, .(vel_avg = mean(velocity)),  by = 't_round']
-  d2
+velocity_avg_enclosed <- function(data, time_window_length=10) {
+
+  data <- prepare_data_for_motion_detector(
+    data,
+    c("t", "xy_dist_log10x1000"),
+    time_window_length
+  )
+
+  data[, dt := c(NA, diff(t))]
+  data[, dist := 10**((xy_dist_log10x1000)/1e3)]
+  data[, velocity :=  dist / dt]
+  #t_round is included in the columns because it is in the by arg
+  data <- data[, .(vel_avg = mean(velocity)),  by = 't_round']
+  return(data)
 }
-attr(velocity_avg, "needed_columns") <- function(...) {
+attr(velocity_avg_enclosed, "needed_columns") <- function(...) {
   c("t", "vel_avg")
 }
+
+movement_detector_enclosed <- function(data, time_window_length=10, threshold=1) {
+
+  # data$core_movement <- data$xy_dist_log10x1000
+  d <- prepare_data_for_motion_detector(data,
+                                        c("t", "core_movement", "x"),
+                                        time_window_length,
+                                        "has_interacted")
+
+  d[,dt := c(NA, diff(t))]
+  #d[,surface_change := xor_dist * 1e-3]
+
+  # restore the distance from the log-transformed variable
+  d[, movement := 10 ^ (core_movement / 1000) ]
+
+  # Get a central summary value for variables of interest
+  # for each window given by t_round
+  # See prepare_data_for_motion_detector to learn
+  # how is t_round computed
+  # velocity_corrected -> max
+  # has_interacted -> sum
+  # beam_cross -> sum
+  d_small <- d[,.(
+    max_movement = max(movement[2:.N])
+  ), by = "t_round"]
+
+  # Gist of the program!!
+  # Score movement as TRUE/FALSE value for every window
+  # Score is TRUE if max_velocity of the window is > 1
+  # Score FALSE otherwise
+  d_small[, micromovement :=  ifelse(max_movement > threshold, TRUE,FALSE)]
+
+  # Set t_round as the representative time of the window
+  # i.e. t becomes the begining of the window and not the t
+  # of the first frame in the window
+  return(d_small)
+}
+attr(movement_detector_enclosed, "needed_columns") <- function(...) {
+  c("t", "max_movement", "micromovement")
+}
+
+
 #' Custom annotation from the dt_raw file
 #'
 #' This function gives aggregates a variable of interest in a custom way
@@ -50,8 +93,7 @@ attr(velocity_avg, "needed_columns") <- function(...) {
 #' @seealso
 #' @import logging
 #' @export
-custom_annotation_closure <- function(custom_function) {
-
+custom_annotation_wrapper <- function(custom_function) {
 
   custom_annotation <- function(data,
                                 time_window_length = 10, #s
@@ -66,7 +108,6 @@ custom_annotation_closure <- function(custom_function) {
       if(nrow(d) < 100)
         return(NULL)
       # todo if t not unique, stop
-
       d_small <- custom_function(d, time_window_length, ...)
       data.table::setnames(d_small, "t_round", "t")
 
@@ -86,7 +127,8 @@ custom_annotation_closure <- function(custom_function) {
       d_small <- stats::na.omit(d[d_small,
                                   on=c("t"),
                                   roll=T])
-      d_small[, intersect(columns_to_keep, colnames(d_small)), with=FALSE]
+      d_small <- d_small[, intersect(columns_to_keep, colnames(d_small)), with=FALSE]
+      return(d_small)
     }
 
     if(is.null(key(data)))
@@ -96,14 +138,12 @@ custom_annotation_closure <- function(custom_function) {
          by=key(data)]
   }
 
-  attr(custom_annotation, "needed_columns") <- attr(custom_function, 'needed_columns')()
+  attr(custom_annotation, "needed_columns") <- function() {attr(custom_function, 'needed_columns')()}
 
   return(custom_annotation)
 }
 
 
-# distance_sum_annotation <- custom_annotation_closure(custom_function = distance_sum)
-# velocity_average_annotation <- custom_annotation_closure(custom_function = velocity_avg())
-
-#
-# attr(velocity_average_annotation, "needed_columns") <- c("t", "vel_avg")
+velocity_avg <- custom_annotation_wrapper(velocity_avg_enclosed)
+dist_sum <- custom_annotation_wrapper(distance_sum_enclosed)
+movement_detector <- custom_annotation_wrapper(movement_detector_enclosed)
