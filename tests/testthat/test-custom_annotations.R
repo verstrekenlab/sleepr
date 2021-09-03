7775e1541c93d01c0475b34be3f70570a53983b2
antortjim <ntoniohu@gmail.com>
2021-09-03 16:00:49 +0200

    Improve code

--- tests/testthat/test-custom_annotations.R
+++ tests/testthat/test-custom_annotations.R
@@ -3,8 +3,8 @@ test_that("Custom annotation works", {
  dt_raw <- behavr::toy_ethoscope_data()
  dt <- sum_movement_detector(dt_raw, threshold=0.5)
  expect_equal(
-         sum(10**((dt_raw$xy_dist_log10x1000)/1000)[1:20]),
-         dt$sum_movement[1]
+         dt_raw[1:20, sum(10**(xy_dist_log10x1000/1000))],
+         dt[1, sum_movement]
  )
 
 })
