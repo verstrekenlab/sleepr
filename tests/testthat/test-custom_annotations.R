08955f5715c6ea589164b682b037eda8da43e28a
antortjim <ntoniohu@gmail.com>
2021-09-03 15:58:30 +0200

    Improve test

--- tests/testthat/test-custom_annotations.R
+++ tests/testthat/test-custom_annotations.R
@@ -1,14 +1,10 @@
 test_that("Custom annotation works", {
 
- dt <- behavr::toy_ethoscope_data()
- distance_sum_enclosed(dt)
-
- # dt1 <- sleepr::custom_annotation_wrapper(sleepr::distance_sum_enclosed)(dt)
- dt2 <- sum_movement_detector(dt, threshold=0.5)
-
- sleep_annotation(dt)
- args <- list(data=dt, FUN=list(sum_movement_detector), time_window_length=10)
- dt <- do.call(scopr::annotate_all, args)
- expect_true("micromovement" %in% colnames(dt))
+ dt_raw <- behavr::toy_ethoscope_data()
+ dt <- sum_movement_detector(dt_raw, threshold=0.5)
+ expect_equal(
+         sum(10**((dt_raw$xy_dist_log10x1000)/1000)[1:20]),
+         dt$sum_movement[1]
+ )
 
 })
